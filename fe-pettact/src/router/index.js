070f95376ec996c5bf64c022f469922c25b1ebe2
lynlim0904 <lynlim0904@gmail.com>
2025-07-20 16:29:17 +0900

    Vue Router - 박규림

---
+++ fe-pettact/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import userRoutes from "@/router/user/userRoutes";
+import mypageRoutes from '@/router/user/mypageRoutes'
+import adminRoutes from '@/router/admin/adminRoutes';
+import productRoutes from "./product/productRoutes";
+import petRoutes from "./pet/petRoutes";
+import boardRoutes from "./board/boardRoutes";
+import categoryRoutes from "./board/categoryRoutes";
+import userPetRoutes from "./pet/userPetRoutes";
+
+const routes = [
+  ...userRoutes,
+  ...mypageRoutes,
+  ...adminRoutes,
+
+  ...productRoutes,
+  ...petRoutes,
+  ...userPetRoutes,
+
+  ...boardRoutes,
+  ...categoryRoutes
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+
+  routes,
+})
+
+export default router;
